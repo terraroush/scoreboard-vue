7c5a7383ee93b607d8ce6825bad92ac726d1b93e
Terra Roush <terra.roush@gmail.com>
2021-03-04 16:40:47 -0600

    winning class

--- src/components/MatchupTitle.vue
+++ src/components/MatchupTitle.vue
@@ -1,14 +1,21 @@
 <template>
-  <h1>{{teamA.name + ' vs ' + teamB.name}}</h1>
+  <h1>
+      <span :class="{winning: teamA.score > teamB.score}" >{{teamA.name}}</span>
+      vs
+      <span :class="{winning: teamB.score > teamA.score}" >{{teamB.name}}</span>
+
+  </h1>
 </template>
 
 <script>
 export default {
-    props: {teamA: Object, teamB: Object}
+    props: {teamA: Object, teamB: Object},
 
 }
 </script>
 
 <style>
-
+.winning {
+    color: blue;
+}
 </style>
