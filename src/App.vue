ce5e2b7df6015c1b27db4e9fb92acab760e73b17
Terra Roush <terra.roush@gmail.com>
2021-03-06 20:57:27 -0600

    final bits

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <matchup-title :teamA="team1" :teamB="team2"></matchup-title>
-    <game-status :teamA="team1" :teamB="team2"></game-status>
-    <div>
-      <score-card :score="team1.score" @addPoint="updateTeamScore(team1, 1)" @subPoint="updateTeamScore(team1, -1)"></score-card>
-      <score-card :score="team2.score" @addPoint="updateTeamScore(team2, 1)" @subPoint="updateTeamScore(team2, -1)"></score-card>
+    <div class="scores">
+      <score-card class="score-card" :score="team1.score" @addPoint="updateTeamScore(team1, 1)" @subPoint="updateTeamScore(team1, -1)"></score-card>
+      <score-card class="score-card" :score="team2.score" @addPoint="updateTeamScore(team2, 1)" @subPoint="updateTeamScore(team2, -1)"></score-card>
     </div>
+    <game-status :teamA="team1" :teamB="team2"></game-status>
   </div>
 </template>
 
@@ -19,12 +19,14 @@ export default {
   data() {
     return {
       team1: {
-        name: "Team 1",
-        score: 0
+        name: "Team Edward",
+        score: 0,
+        pic: "https://media.apnarm.net.au/media/images/2020/05/05/v3imagesbincf2005744468c915c4ef7bf8fa64c989-7movkwqsvd1rwtn3au2_t1880.jpg"
       },
       team2: {
-        name: "Team 2",
-        score: 0
+        name: "Team Jacob",
+        score: 0,
+        pic: "https://openpsychometrics.org/tests/characters/test-resources/pics/T/3.jpg"
       }
     }
   },
@@ -48,5 +50,15 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+}
+.scores {
+  display: flex;
+  justify-content: center;
+  
+}
+.score-card {
+  padding: 50px;
 }
 </style>
--- src/components/GameStatus.vue
+++ src/components/GameStatus.vue
@@ -1,28 +1,42 @@
 <template>
-<div>
-  <p v-if="gameIsTied">The game is tied</p>
+<div >
+  <p v-if="gameIsTied">The really silly game is tied</p>
   <p v-else>{{gameLeaderDisplay}}</p>
+  <div>
+    <img v-if="gameIsTied" src="https://novelnovicetwilight.files.wordpress.com/2008/07/cullen-vs-black_the-debate8.jpg" />
+    <img v-else :src="leaderPicDisplay" />
+  </div>
+  
 </div>
 </template>
 
 <script>
 export default {
  props: {teamA: Object, teamB: Object},
+
  computed: {
      gameIsTied() {
-      return this.$props.teamA.score === this.$props.teamB.score;
+      return this.teamA.score === this.teamB.score;
     },
      gameLeaderDisplay() {
-        const { teamA, teamB } = this.$props;
-            const leadingTeam = teamA.score > teamB.score ? teamA : teamB;
-            const lead = Math.abs(teamA.score - teamB.score);
-            const points = lead === 1 ? "point" : "points";
-            return `${leadingTeam.name} is leading by ${lead} ${points}`;
+            const leadingTeam = this.teamA.score > this.teamB.score ? this.teamA : this.teamB;
+            const losingTeam = this.teamA.score > this.teamB.score ? this.teamB : this.teamA;
+            const numberLead = Math.abs(this.teamA.score - this.teamB.score);
+            const pointOrPoints = numberLead === 1 ? "point" : "points";
+            const leadCrushOrKill = numberLead >= 1 && numberLead < 3 ? "leading" : numberLead >= 3 && numberLead < 10 ? "crushing" : "killing";
+            return `${leadingTeam.name} is ${leadCrushOrKill} ${losingTeam.name} by ${numberLead} ${pointOrPoints}`;
+     },
+     leaderPicDisplay() {
+         const leader = this.teamA.score > this.teamB.score ? this.teamA : this.teamB;
+         return `${leader.pic}`
      }
  }
 }
 </script>
 
 <style>
+img {
+    width: 600px;;
+}
 
 </style>
